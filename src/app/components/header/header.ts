f2846ebc9358f10cc07bae115d161549f4912253
Neeraali-digital <neeraalidigitalweb@gmail.com>
2026-03-25 10:43:55 +0530

    powered by , mobile view

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
